551434803ff42a67f7b42f533eaeed19be785de3
mph55 <morphi@waifu.club>
2019-09-23 17:53:32 -0300

    maptools

--- maptools/1preparar_mini2.bat
+++
@@ -1,6 +0,0 @@
-set MAPFILE_MINI=mini2.dmm
-
-cd ../maps
-copy %MAPFILE_MINI% %MAPFILE_MINI%.backup
-
-pause
--- maptools/2limpar_mini2.bat
+++
@@ -1,5 +0,0 @@
-set MAPFILE_MINI=mini2.dmm
-
-java -jar MapPatcher.jar -clean ../maps/%MAPFILE_MINI%.backup ../maps/%MAPFILE_MINI% ../maps/%MAPFILE_MINI%
-
-pause
